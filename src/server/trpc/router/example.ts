3928ad7aefb3664be263e09cba8228843dd969dd
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2023-03-05 20:24:22 +0530

    fix: more updates

--- src/server/trpc/router/example.ts
+++ src/server/trpc/router/example.ts
@@ -1,13 +1,23 @@
+import { readFileSync } from "fs";
 import { z } from "zod";
 
 import { router, publicProcedure } from "../trpc";
 
+interface File {
+  name: string;
+  versions: string[];
+}
+
+interface Folder {
+  name: string;
+  files: File;
+  folders: Folder[];
+}
+
 export const exampleRouter = router({
-  hello: publicProcedure
-    .input(z.object({ text: z.string().nullish() }).nullish())
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input?.text ?? "world"}`,
-      };
-    }),
+  hello: publicProcedure.query(() => {
+    return JSON.parse(
+      readFileSync("public/cdn-source/manifest.json").toString()
+    ) as Folder;
+  }),
 });
