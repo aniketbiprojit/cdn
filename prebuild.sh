306cbb8a28e645c70c22b5e7a8064c6dc7936ef4
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2023-03-05 20:08:44 +0530

    fix: prebuild

--- prebuild.sh
+++ prebuild.sh
@@ -4,8 +4,17 @@ git submodule update --remote --merge
 
 cd ./public/cdn-source
 
+echo "deleting 1"
 rm -rf Cargo*
+echo "deleting 2"
 rm -rf ./.gitignore
+echo "deleting 3"
 rm -rf src
+echo "deleting 4"
+rm -rf .git
 
+echo "more"
 cd ../..
+
+
+echo "Exiting"
