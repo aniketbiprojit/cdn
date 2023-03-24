6538b2c616e296954444bd299c3b93d42f265025
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2023-03-24 12:29:41 +0530

    feat: move-to-ssr

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,36 @@
-import { type NextPage } from "next";
+import {
+  GetServerSideProps,
+  InferGetServerSidePropsType,
+  type NextPage,
+} from "next";
 import Head from "next/head";
-import Link from "next/link";
-import { signIn, signOut, useSession } from "next-auth/react";
 
-import { trpc } from "../utils/trpc";
 import { Folder } from "../server/trpc/router/example";
 import { useEffect, useState } from "react";
+import { readFileSync } from "fs";
 
-const Home: NextPage = () => {
-  const run = async () => {
-    const res = await fetch("/cdn-source/manifest.json");
-    const data: Folder = await res.json();
-    setManifest(data);
-    return data;
+export const getServerSideProps: GetServerSideProps<{
+  data: Folder;
+}> = async () => {
+  const res = JSON.parse(
+    readFileSync("public/cdn-source/manifest.json").toString()
+  );
+  const data: Folder = res;
+  return {
+    props: { data },
   };
+};
 
+const Home: NextPage<
+  InferGetServerSidePropsType<typeof getServerSideProps>
+> = ({ data }) => {
   const [manifest, setManifest] = useState<Folder>();
 
   useEffect(() => {
     if (manifest === undefined) {
-      run();
+      setManifest(data);
     }
-  }, []);
+  }, [data]);
 
   return (
     <>
@@ -35,58 +44,3 @@ const Home: NextPage = () => {
 };
 
 export default Home;
-
-const AuthShowcase: React.FC = () => {
-  const { data: sessionData } = useSession();
-
-  const { data: secretMessage } = trpc.auth.getSecretMessage.useQuery(
-    undefined, // no input
-    { enabled: sessionData?.user !== undefined }
-  );
-
-  return (
-    <div className="flex flex-col items-center justify-center gap-2">
-      {sessionData && (
-        <p className="text-2xl text-blue-500">
-          Logged in as {sessionData?.user?.name}
-        </p>
-      )}
-      {secretMessage && (
-        <p className="text-2xl text-blue-500">{secretMessage}</p>
-      )}
-      <button
-        className="rounded-md border border-black bg-violet-50 px-4 py-2 text-xl shadow-lg hover:bg-violet-100"
-        onClick={sessionData ? () => signOut() : () => signIn()}
-      >
-        {sessionData ? "Sign out" : "Sign in"}
-      </button>
-    </div>
-  );
-};
-
-type TechnologyCardProps = {
-  name: string;
-  description: string;
-  documentation: string;
-};
-
-const TechnologyCard: React.FC<TechnologyCardProps> = ({
-  name,
-  description,
-  documentation,
-}) => {
-  return (
-    <section className="flex flex-col justify-center rounded border-2 border-gray-500 p-6 shadow-xl duration-500 motion-safe:hover:scale-105">
-      <h2 className="text-lg text-gray-700">{name}</h2>
-      <p className="text-sm text-gray-600">{description}</p>
-      <Link
-        className="m-auto mt-3 w-fit text-sm text-violet-500 underline decoration-dotted underline-offset-2"
-        href={documentation}
-        target="_blank"
-        rel="noreferrer"
-      >
-        Documentation
-      </Link>
-    </section>
-  );
-};
